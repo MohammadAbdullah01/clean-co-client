bf55a53ef127481710ad22a37889a0176cf3b385
Mohammad-Abdullah <abdullahofficial2022@gmail.com>
2022-06-01 14:11:36 +0600

    basic router setuped and navbar completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Navbar from "./pages/Navbar";
+import Services from "./pages/Services";
+
 function App() {
   return (
     <div className="App">
-
+      <Navbar>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/about' element={<About></About>}></Route>
+          <Route path='/services' element={<Services></Services>}></Route>
+          <Route path='/contact' element={<Contact></Contact>}></Route>
+          <Route path='/login' element={<Login></Login>}></Route>
+        </Routes>
+      </Navbar>
     </div>
   );
 }
