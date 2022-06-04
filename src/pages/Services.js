6d2fea1bd48f0f67ac14b638a8dc7b3ad9916047
Mohammad-Abdullah <abdullahofficial2022@gmail.com>
2022-06-04 21:42:17 +0600

    server connected and axios initialized

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import fetcher from '../api';
 
 const Services = () => {
+    const [services, setServices] = useState([])
+    useEffect(() => {
+        // axios.get('http://localhost:5000/service')
+        //     .then(res => setServices(res.data))
+        (async () => {
+            const res = await fetcher.get('/service')
+            setServices(res.data);
+        })()
+    }, [])
+    console.log(services);
     return (
         <div>
             <h1>services </h1>
